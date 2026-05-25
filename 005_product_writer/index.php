6da737af4da9d0879fe08db08fccf26dc9d76ac3
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-25 17:51:45 +0200

    Lesson 005: Code simplified and comments added.

--- 005_product_writer/index.php
+++ 005_product_writer/index.php
@@ -1,64 +1,65 @@
 <?php
-    class ShopProduct {
-        public $title = "Test product";
-        public $producerMainName = "Nachname";
-        public $producerFirstName = "Vorname";
-        public $price = 0;
+class ShopProduct
+{
 
-        public function __construct(
-            string $title = "Test product",
-            string $producerMainName = "Nachname",
-            string $producerFirstName = "Vorname",
-            float $price = 0
-        ) {
-            $this->title = $title;
-            $this->producerMainName = $producerMainName;
-            $this->producerFirstName = $producerFirstName;
-            $this->price = $price;
-        }
+    public function __construct(
+        public string $title = "Default product",
+        public string $producerMainName = "Nachname",
+        public string $producerFirstName = "Vorname",
+        public float $price = 0
+    ) {}
 
-        public function getProducer () {
-           return "{$this->producerFirstName} " . "{$this->producerMainName}";
-        }
-    }
-
-    class ShopProductWriter
+    public function getProducer()
     {
-        public function write(ShopProduct $shopProduct)
-        {
-            $str = "<pre>" . $shopProduct->title . ": " . $shopProduct->getProducer() . " (" . $shopProduct->price . ")\n" . "</pre>";
-            print $str;
-        }
+        return "{$this->producerFirstName} " . "{$this->producerMainName}";
     }
+}
 
-    $product1 = new ShopProduct(
-        "Cобачье сердце",
-        "Михаил",
-        "Булгаков",
-        5.99,
-    );
-    $writer = new ShopProductWriter();
-    $product2 = new ShopProduct();
+class ShopProductWriter
+{
+    /**
+     * Выводит информацию о книге в HTML-формате.
+     *
+     * Формирует строку с названием произведения, именем автора
+     * и стоимостью книги, после чего выводит её на экран внутри
+     * тега <pre> для сохранения форматирования текста.
+     *
+     * @param ShopProduct $shopProduct Объект книги, содержащий название,
+     *                                 автора и цену.
+     *
+     * @return void
+     */
+    public function write(ShopProduct $shopProduct)
+    {
+        $str = "<pre>" . $shopProduct->title . ": " . $shopProduct->getProducer() . " (" . $shopProduct->price . ")\n" . "</pre>";
+        print $str;
+    }
+}
 
-    
-    $product2->title = "Ревизор";
+$product1 = new ShopProduct(
+    "Cобачье сердце",
+    "Михаил",
+    "Булгаков",
+    5.99,
+);
+$writer = new ShopProductWriter();
+$product_default = new ShopProduct();
 
-    print $product1->title . "<br>";
-    print $product2->title . "<br>";
+print $product1->title . "<br>";
 
-    print "<hr>";
+print "<hr>";
 
-    print "<b>Function</b> getProducer() : <br>";
+print "<b>Function</b> ShopProduct::getProducer() : <br>";
 
-    print "<pre>Автор: " . $product1->getProducer() ."</pre><br>";    
+print "<pre>Автор: " . $product1->getProducer() . "</pre><br>";
 
-    print "<b>Function</b> ShopProductWriter::write() : <br>";
+print "<b>Function</b> ShopProductWriter::write() : <br>";
 
-    $writer->write($product1);
+$writer->write($product1);
 
-    print "<hr><br>";
+print "<hr><br>";
 
-    echo "<pre>";
-    var_dump($product1);
-    var_dump($product2);
-    echo "</pre>";
+echo "<pre>";
+var_dump($product1);
+var_dump($product_default);
+echo "</pre>";
