64df6356ad2dd2cfb80970047d70d90247ff5cdb
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-25 02:11:58 +0200

    Added examples showing changes to class constructors in PHP 8

--- 003_construct/index.php
+++ 003_construct/index.php
@@ -1,51 +1,85 @@
 <?php
-    class ShopProduct {
-        public $title = "Test product";
-        public $producerMainName = "Nachname";
-        public $producerFirstName = "Vorname";
-        public $price = 0;
+class ShopProduct
+{
+    public $title = "Test product";
+    public $producerMainName = "Nachname";
+    public $producerFirstName = "Vorname";
+    public $price = 0;
 
-        public function __construct(
-            $title = "Test product",
-            $producerMainName = "Nachname",
-            $producerFirstName = "Vorname",
-            $price = 0
-        ) {
-            $this->title = $title;
-            $this->producerMainName = $producerMainName;
-            $this->producerFirstName = $producerFirstName;
-            $this->price = $price;
-        }
-
-        public function getProducer () {
-            print "<b>Function</b> getProducer(): <pre>";
-            print "Автор: {$this->producerFirstName} " . 
-            "{$this->producerMainName}\n";
-            print "</pre>";
-        }
+    public function __construct(
+        $title = "Test product",
+        $producerMainName = "Nachname",
+        $producerFirstName = "Vorname",
+        $price = 0
+    ) {
+        $this->title = $title;
+        $this->producerMainName = $producerMainName;
+        $this->producerFirstName = $producerFirstName;
+        $this->price = $price;
     }
 
-    $product1 = new ShopProduct(
-        "Cобачье сердце",
-        "Михаил",
-        "Булгаков",
-        5.99,
-    );
-    $product2 = new ShopProduct();
+    public function getProducer()
+    {
+        print "<b>Function</b> ShopProduct::getProducer(): <pre>";
+        print "Автор: {$this->producerFirstName} " .
+            "{$this->producerMainName}\n";
+        print "</pre>";
+    }
+}
 
-    
-    $product2->title = "Ревизор";
+class ShopProduct_PHP8
+{
+    public function __construct(
+        public $title = "Test product",
+        public $producerMainName = "Nachname",
+        public $producerFirstName = "Vorname",
+        public $price = 0
+    ) {}
 
-    print $product1->title . "<br>";
-    print $product2->title . "<br>";
+    public function getProducer()
+    {
+        print "<b>Function</b> ShopProduct_PHP8::getProducer(): <pre>";
+        print "Автор: {$this->producerFirstName} " .
+            "{$this->producerMainName}\n";
+        print "</pre>";
+    }
+}
 
-    print "<hr>";
+$product1 = new ShopProduct(
+    "Cобачье сердце",
+    "Михаил",
+    "Булгаков",
+    5.99,
+);
+$product2 = new ShopProduct_PHP8(
+    "Ревизор",
+    "Николай",
+    "Гоголь",
+    4.99,
+);
+$product_default = new ShopProduct_PHP8();
+$product_title_and_price = new ShopProduct_PHP8(
+    price: 9.99,
+    title: "Мастер и Маргарита",
+);
 
-    $product1->getProducer();    
 
-    print "<hr><br>";
+print $product1->title . "<br>";
+print $product2->title . "<br>";
 
-    echo "<pre>";
-    var_dump($product1);
-    var_dump($product2);
-    echo "</pre>";
+print "<hr>";
+
+$product1->getProducer();
+
+print "<hr>";
+
+$product2->getProducer();
+
+print "<hr><br>";
+
+echo "<pre>";
+var_dump($product1);
+var_dump($product2);
+var_dump($product_default);
+var_dump($product_title_and_price);
+echo "</pre>";
