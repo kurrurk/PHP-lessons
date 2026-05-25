7dd1eb5cab11ad29fbae6e2bc5c9a2ebcd5a3645
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-25 16:46:23 +0200

    Added a PHP 7-style code example and comments explaining the behavior of the library functions used.

--- 004_bool-type_error/index.php
+++ 004_bool-type_error/index.php
@@ -1,28 +1,51 @@
 <?php
-    class AddressManager {
-        private $addresses = ["209.131.36.159", "216.58.213.174"];
-        /**
-          * Вывести список адресов.
-          * Если переменная $resolve содержит истинное
-          * значение (true), то адрес преобразуется в
-          * эквивалентное имя хоста.
-          * @param $resolve Boolean Преобразовать адрес?
-          */
-        public function outputAddresses(bool $resolve)
-        {
-            foreach ($this->addresses as $address) {
-                print $address;
-                if ($resolve) {
-                    print " (" . gethostbyaddr($address) . ")";
-                }
-                print "<br />\n";
+class AddressManager
+{
+    private $addresses = ["209.131.36.159", "216.58.213.174"];
+    /**
+     * Вывести список адресов.
+     * Если переменная $resolve содержит истинное
+     * значение (true), то адрес преобразуется в
+     * эквивалентное имя хоста.
+     * @param $resolve Boolean Преобразовать адрес?
+     */
+    public function outputAddresses(bool $resolve)
+    {
+        foreach ($this->addresses as $address) {
+            print $address;
+            if ($resolve) {
+                print " (" . gethostbyaddr($address) . ")"; // Получить имя хоста по IP-адресу
             }
+            print "<br />\n";
         }
-    }    
+    }
 
-    $settings = simplexml_load_file(__DIR__ . "/resolve.xml");
-    $manager = new AddressManager();
-    $resolve = (preg_match("/^(false|no|off)$/i", $settings->resolvedomains)) ? false : true;
-    $manager->outputAddresses( $resolve );
-    echo "-----------------------------<br/>\n";
-    $manager->outputAddresses( true );
+    /**
+     * Вывести список адресов.
+     * Если переменная $resolve содержит истинное
+     * значение (true), то адрес преобразуется в
+     * эквивалентное имя хоста.
+     * @param $resolve Boolean Преобразовать адрес?
+     */
+    public function outputAddresses_PHP7($resolve)
+    {
+        foreach ($this->addresses as $address) {
+            print $address;
+            if (!is_bool($resolve)) {
+                print "<br />\n";
+                return;
+            }
+            if ($resolve) {
+                print " (" . gethostbyaddr($address) . ")"; // Получить имя хоста по IP-адресу
+            }
+            print "<br />\n";
+        }
+    }
+}
+
+$settings = simplexml_load_file(__DIR__ . "/resolve.xml");
+$manager = new AddressManager();
+$resolve = (preg_match("/^(false|no|off)$/i", $settings->resolvedomains)) ? false : true;
+$manager->outputAddresses($resolve);
+echo "-----------------------------<br/>\n";
+$manager->outputAddresses(true);
