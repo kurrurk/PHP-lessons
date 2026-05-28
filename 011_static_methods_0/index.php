a9c67a587e41184ba2c6f39ba525d5c9a5879f93
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-28 18:21:02 +0200

    Lesson 011: Added a second example demonstrating the use of the self keyword

--- 011_static_methods_0/index.php
+++ 011_static_methods_0/index.php
@@ -2,12 +2,26 @@
 
 class StaticExample
 {
-    public static $aNum = "Здравствуй, Мир - 1!";
+    public static $aNum = 'Здравствуй, Мир - 1!';
+
     public static function sayHello()
     {
-        print "Здравствуй, Мир - 2!";
+        echo 'Здравствуй, Мир - 2!<br/>';
     }
 }
 
-echo StaticExample::$aNum . "<br/>";
+echo StaticExample::$aNum.'<br/>';
 StaticExample::sayHello();
+
+class StaticExample2
+{
+    public static $bNum = 2;
+
+    public static function sayHello()
+    {
+        ++self::$bNum;
+        echo 'Здравствуй, Мир - '.self::$bNum.'!<br/>';
+    }
+}
+
+StaticExample2::sayHello();
