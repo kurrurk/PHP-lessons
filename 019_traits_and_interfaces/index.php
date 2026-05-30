a6aee8b96f514ad5d16e6aaee7533b51d8d54b01
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-30 10:58:25 +0200

    Lesson 019: Combination of Traits and Interfaces

---
+++ 019_traits_and_interfaces/index.php
@@ -0,0 +1,94 @@
+<?php
+
+trait PriceUtilities
+{
+    private int $taxrate = 20;
+
+    /**
+     * @return int
+     */
+    public function calculateTax(float $price): float
+    {
+        return ($this->taxrate / 100) * $price;
+    }
+    // Other utility methods related to price calculations can be added here
+}
+
+interface IdentityObject
+{
+    public function generateId(): string;
+}
+
+trait IdentityTrait
+{
+    public function generateId(): string
+    {
+        return uniqid();
+    }
+}
+
+class ShopProduct implements IdentityObject
+{
+    use PriceUtilities;
+    use IdentityTrait;
+}
+
+abstract class Service
+{
+    abstract public function doSomething(): void;
+}
+
+class UtilityService extends Service
+{
+    use PriceUtilities;
+
+    public function doSomething(): void
+    {
+        // Implementation of the abstract method
+    }
+}
+
+function getReflection(string $classname): ReflectionClass
+{
+    $reflection = new ReflectionClass($classname);
+
+    echo 'Class: ' . $reflection->getName() . PHP_EOL;
+    if ($reflection->getParentClass()) {
+        echo 'Parent: ' . $reflection->getParentClass()?->getName() . PHP_EOL;
+    }
+
+    echo PHP_EOL . 'Interfaces:' . PHP_EOL;
+
+    foreach ($reflection->getInterfaceNames() as $interface) {
+        echo ' - ' . $interface . PHP_EOL;
+    }
+
+    echo '<br/><hr><br/>';
+    return new ReflectionClass($classname);
+}
+
+$shopProduct = new ShopProduct();
+$utilityService = new UtilityService();
+
+// тело программы
+
+print '<strong>$shopProduct</strong>->calculateTax(100): ' . $shopProduct->calculateTax(100) . "<br>";
+print '<strong>$utilityService</strong>->calculateTax(100): ' . $utilityService->calculateTax(100) . "<br>";
+
+print "<hr>";
+
+print '<strong>$shopProduct</strong>->generateId(): ' . $shopProduct->generateId() . "<br>";
+
+print "<hr><br>";
+
+echo "<pre>";
+var_dump($shopProduct);
+var_dump($utilityService);
+echo "</pre>";
+
+print "<hr>";
+
+echo "<pre>";
+getReflection(ShopProduct::class);
+getReflection(UtilityService::class);
+echo "</pre>";
