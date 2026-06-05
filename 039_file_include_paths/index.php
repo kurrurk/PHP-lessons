05cd518f480625aee6cd2cad3f869fe9f2071872
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-06 00:40:56 +0200

    Lesson 039: Visual enhancements.

--- 039_file_include_paths/index.php
+++ 039_file_include_paths/index.php
@@ -1,14 +1,18 @@
 <?php
 
+echo "----------------------------<br/>\n";
 echo "До изменения:<br>";
-echo get_include_path() . "<br><br>";
+echo get_include_path() . "<br>";
+echo "----------------------------<br/>\n";
 
 set_include_path(
     get_include_path() . PATH_SEPARATOR . __DIR__ . "/library"
 );
 
+echo "----------------------------<br/>\n";
 echo "До изменения:<br>";
-echo get_include_path() . "<br><br>";
+echo get_include_path() . "<br>";
+echo "----------------------------<br/>\n";
 
 require_once "MyClass.php";
 
